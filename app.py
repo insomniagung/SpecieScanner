6b4ca7aff1a81418b165dcf48a49bbdae8ba394c
Agung Gunawan <59593824+insomniagung@users.noreply.github.com>
2023-02-03 00:37:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -25,55 +25,54 @@ def main():
             return welcome
         
     elif app_menu == 'Scan' :
-    st.header("Pemindai Spesies Bunga Iris Menggunakan Algoritma K-Nearest Neighbour (KNN)")
-    st.markdown("By: Agung Gunawan")
-    st.write('\n')
-    st.image("images/irisflowers.png", width=700)
-    st.write('\n')
-    st.write('\n')
+        st.header("Pemindai Spesies Bunga Iris Menggunakan Algoritma K-Nearest Neighbour (KNN)")
+        st.markdown("By: Agung Gunawan")
+        st.write('\n')
+        st.image("images/irisflowers.png", width=700)
+        st.write('\n')
+        st.write('\n')
     
-    st.sidebar.title("SpecieScanner Apps")
-    st.sidebar.write('\n')
-    st.sidebar.write('\n')
-    st.sidebar.markdown("<strong>Sebelum memindai, isi data berikut :</strong>", 
-                        unsafe_allow_html=True)
-    SepalLengthCm = st.sidebar.number_input(label="Sepal Length (cm)", step=1., format="%.1f")
-    SepalWidthCm = st.sidebar.number_input(label="Sepal Width (cm)", step=1., format="%.1f")
-    PetalLengthCm = st.sidebar.number_input(label="Petal Length (cm)", step=1., format="%.1f")
-    PetalWidthCm = st.sidebar.number_input(label="Petal Width (cm)", step=1., format="%.1f")
-    result =""
+        st.sidebar.title("SpecieScanner Apps")
+        st.sidebar.write('\n')
+        st.sidebar.write('\n')
+        st.sidebar.markdown("<strong>Sebelum memindai, isi data berikut :</strong>", unsafe_allow_html=True)
     
-    st.write('\n')
-    if st.button("PINDAI SPESIES"):
-        result = prediction(SepalLengthCm, SepalWidthCm, PetalLengthCm, PetalWidthCm)
-        for result_predict in result:
-            print(result_predict)
+        SepalLengthCm = st.sidebar.number_input(label="Sepal Length (cm)", step=1., format="%.1f")
+        SepalWidthCm = st.sidebar.number_input(label="Sepal Width (cm)", step=1., format="%.1f")
+        PetalLengthCm = st.sidebar.number_input(label="Petal Length (cm)", step=1., format="%.1f")
+        PetalWidthCm = st.sidebar.number_input(label="Petal Width (cm)", step=1., format="%.1f")
+        result =""
+    
+        st.write('\n')
+        if st.button("PINDAI SPESIES"):
+            result = prediction(SepalLengthCm, SepalWidthCm, PetalLengthCm, PetalWidthCm)
+            for result_predict in result:
+                print(result_predict)
             
-        if result_predict == 'Iris-setosa':
-            result_predict = 'Iris Setosa'
-        elif result_predict == 'Iris-versicolor':
-            result_predict = 'Iris Versicolor'
-        else:
-            result_predict = 'Iris Virginica'
+            if result_predict == 'Iris-setosa':
+                result_predict = 'Iris Setosa'
+            elif result_predict == 'Iris-versicolor':
+                result_predict = 'Iris Versicolor'
+            else:
+                result_predict = 'Iris Virginica'
         
-        st.write('\n')
-        st.write('\n')
-        st.write('\n')
-        with st.spinner('Sedang Memindai...'):
-            time.sleep(2)
-        st.success(f'Sukses, berdasarkan sepal dan petal, iris termasuk spesies {result_predict}.')
+            st.write('\n')
+            st.write('\n')
+            st.write('\n')
+            with st.spinner('Sedang Memindai...'):
+                time.sleep(2)
+            st.success(f'Sukses, berdasarkan sepal dan petal, iris termasuk spesies {result_predict}.')
         
-        if result_predict == "Iris Setosa":
-            st.image("images/iris-setosa.png", width=350)
-        elif result_predict == "Iris Versicolor":
-            st.image("images/iris-versicolor.png", width=350)
-        else:
-            st.image("images/iris-virginica.png", width=350)
+            if result_predict == "Iris Setosa":
+                st.image("images/iris-setosa.png", width=350)
+            elif result_predict == "Iris Versicolor":
+                st.image("images/iris-versicolor.png", width=350)
+            else:
+                st.image("images/iris-virginica.png", width=350)
             
-if __name__=='__main__':
+ if __name__=='__main__':
     main()
-    
-    
+        
 #menghilangkan burger dan made with streamlit
 hide_streamlit_style = """
 <style>
